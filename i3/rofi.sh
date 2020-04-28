ed29e0b658b3fb59e730de55e6cab467e698312d
Daniil Bondarenko <dan_2015@mail.ru>
2020-04-29 00:00:58 +0300

    rofi setup.

--- i3/rofi.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-rofi -show run -font "Cantarell 15" rofi -color-window "#263238, #263238, #1e2529" \
-  -color-normal "#263238, #c1c1c1, #263238, #2379C2, #ffffff" -o 85 -bw 0 -fuzzy \
-  -lines 10 -quiet -separator-style none -hide-scrollbar
